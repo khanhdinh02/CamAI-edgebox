f2197e909e7fb3dab569e18eb5b5047ac02adccb
ngud <nguyenduduy233@gmail.com>
2024-02-28 15:44:49 +0700

    Task to do

---
+++ CamAIEdgeBox/CamAI.EdgeBox.Services/AI/Detection/DetectionProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace CamAI.EdgeBox.Services.AI.Detection;
+
+public class DetectionProcessor
+{
+    // receive data
+    // cache data
+    // process data
+    // calculate matrix and score
+    // 2% increase
+    // accepted score > 0.75
+    // minimum time: 5 seconds
+    // incident time 3-4 mins
+    // break time 30s
+
+    // send evidence
+}
