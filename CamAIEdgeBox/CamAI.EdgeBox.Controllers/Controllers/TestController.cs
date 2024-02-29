2f52b431c919d5e9542eea75b797a2a5e55c7794
Truong Tien Dat <dd220302@gmail.com>
2024-03-01 06:00:20 +0700

    remove comment code in test controller

--- CamAIEdgeBox/CamAI.EdgeBox.Controllers/Controllers/TestController.cs
+++ CamAIEdgeBox/CamAI.EdgeBox.Controllers/Controllers/TestController.cs
@@ -11,17 +11,6 @@ public class TestController(IPublishEndpoint bus, ILogger<TestController> logger
     [HttpGet("{name}")]
     public async Task<ActionResult<string>> TestConnection([FromRoute] string name)
     {
-        //if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        //{
-        //    //var dir = Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().LastIndexOf("/"));
-        //    //if (Directory.Exists(@$"{dir}/scripts"))
-        //    //    await @$"{dir}/scripts/CpuUsageScript.sh".Bash(logger);
-        //    await $"top -bn1".LinuxCmd(logger);
-        //}
-        //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        //{
-        //    await "wmic cpu get loadpercentage".WindowsPrompt(logger);
-        //}
         logger.LogInformation($"CPU usage: {cache.Get("CpuUsage")}");
         return $"Hello {name} from edge box";
     }
