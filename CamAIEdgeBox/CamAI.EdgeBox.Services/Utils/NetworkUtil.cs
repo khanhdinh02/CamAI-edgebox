25200fc2d87fc0d0bead853a2ff8773b43dc50f2
duy <nguyenduduy233@gmail.com>
2024-04-10 16:08:06 +0700

    Fml

--- CamAIEdgeBox/CamAI.EdgeBox.Services/Utils/NetworkUtil.cs
+++ CamAIEdgeBox/CamAI.EdgeBox.Services/Utils/NetworkUtil.cs
@@ -7,10 +7,11 @@ public static class NetworkUtil
 {
     public static string GetLocalIpAddress()
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
-                return ip.ToString();
-        throw new Exception("No network adapters with an IPv4 address in the system!");
+        using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+        socket.Connect("8.8.8.8", 65530);
+        var endPoint = socket.LocalEndPoint as IPEndPoint;
+        var localIp = endPoint.Address.ToString();
+
+        return localIp;
     }
 }
