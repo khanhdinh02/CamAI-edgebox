6a69ecef4ee7d4bf602f120426d3a3411108aecb
duy <nguyenduduy233@gmail.com>
2024-04-14 09:28:16 +0700

    Fix message queue

--- CamAIEdgeBox/CamAI.EdgeBox.Models/GlobalData.cs
+++ CamAIEdgeBox/CamAI.EdgeBox.Models/GlobalData.cs
@@ -5,7 +5,7 @@ public static class GlobalData
     public static Shop? Shop { get; set; } = null!;
     public static Brand? Brand { get; set; } = null!;
     public static DbEdgeBox? EdgeBox { get; set; } = null!;
-    public static List<Camera> Cameras { get; set; } = null!;
+    public static List<Camera> Cameras { get; set; } = [];
     public static string? Version { get; set; }
     public static string? MacAddress { get; set; }
     public static string? OsName { get; set; }
