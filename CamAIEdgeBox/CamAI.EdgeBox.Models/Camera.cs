e404cc0bc641f7a30c544bed2c5dbf71da97c27a
duy <nguyenduduy233@gmail.com>
2024-04-14 11:38:05 +0700

    Update camera in evidence

--- CamAIEdgeBox/CamAI.EdgeBox.Models/Camera.cs
+++ CamAIEdgeBox/CamAI.EdgeBox.Models/Camera.cs
@@ -4,7 +4,7 @@ namespace CamAI.EdgeBox.Models;
 
 public class Camera : BaseEntity
 {
-    public Guid ShopId { get; set; }
+    public Guid ShopId => GlobalData.Shop!.Id;
 
     [StringLength(255)]
     public string Name { get; set; } = null!;
