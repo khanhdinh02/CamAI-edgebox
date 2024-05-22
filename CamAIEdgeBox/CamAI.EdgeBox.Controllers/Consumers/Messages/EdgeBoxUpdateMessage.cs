128820e709bae5b7219cb629a363ced70b67985d
duy <nguyenduduy233@gmail.com>
2024-05-22 16:09:32 +0700

    Fix get set

--- CamAIEdgeBox/CamAI.EdgeBox.Controllers/Consumers/Messages/EdgeBoxUpdateMessage.cs
+++ CamAIEdgeBox/CamAI.EdgeBox.Controllers/Consumers/Messages/EdgeBoxUpdateMessage.cs
@@ -8,7 +8,7 @@ public class EdgeBoxUpdateMessage : BaseUpdateMessage
     public string? SerialNumber { get; set; } = null!;
     public string? Name { get; set; } = null!;
     public string Model { get; set; } = null!;
-    public int MaxNumberOfRunningAi;
+    public int MaxNumberOfRunningAi { get; set; }
     public EdgeBoxActivationStatus ActivationStatus { get; set; }
 }
 
